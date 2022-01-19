94186e4e29dd6412d261a37bafdf8522b61e6164
Ted <tugrulerdemdogru@gmail.com>
2022-01-19 23:53:58 +0300

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+    <header class="text-center text-white  bg-blue-800 p-4 mb-10">
+        <div class="text-3xl md:text-5xl font-bold mb-3">
+            <i class="fa fa-viruses"></i>
+            Covid 19 Tracker</div>
+        <p>API by </p>
+    </header>
+</template>
+<script>
+export default {
+  name: 'Header',
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,44 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <main class="flex flex-col align-center justify-center text-center" v-if="!loading">
+    Show data...
+  </main>
+
+ <main class="flex flex-col align-center justify-center text-center" v-else>
+   <div class="text-gray-500 text-3l">Fetching data..</div>
+   <img :src="loadingImage" class="mx-auto" width="125" alt="">
+ </main>
+
 </template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
-
 export default {
   name: 'Home',
-  components: {
-    HelloWorld,
+  components: {},
+  data() {
+    return {
+      loading: true,
+      title: 'Global',
+      dataDate: '',
+      stats: {},
+      countries: [],
+      loadingImage: 'https://icon-library.com/images/loading-icon-animated-gif/loading-icon-animated-gif-6.jpg',
+    };
   },
+  methods: {
+    async fetchCovidData() {
+      const res = await fetch('https://api.covid19api.com/summary');
+      const data = await res.json();
+      return data;
+    },
+  },
+  async created() {
+    const data = await this.fetchCovidData();
+    console.log(data);
+
+    this.dataDate = data.Date;
+    this.stats = data.Global;
+    this.countries = data.Countries;
+    this.loading = false;
+  },
+
 };
 </script>
